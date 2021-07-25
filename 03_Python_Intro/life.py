7034cb41c6988be3e2982fbb498f940b053452ec
Matthias Mau <matthias.mau@outlook.com>
2021-07-25 19:54:32 +0200

    recap of previous lesson and notes updated

--- 03_Python_Intro/life.py
+++ 03_Python_Intro/life.py
@@ -4,6 +4,7 @@ theAnswer = 42
 
 def quote_marvin():
     print("I've calculated your chance of survival, but I don't think you'll like it.")
+    print ('random add on to test what I\'ve done')
 
 
 def square_root(x):
